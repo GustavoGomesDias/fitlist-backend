fde3b7d63f62945a98986d480a69825cfa85102b
GustavoCedisa <gustavo.dias@cedisa.com.br>
2023-08-31 16:21:56 -0300

    fix: add DAO implementation in export file

--- src/db/DAO/index.ts
+++ src/db/DAO/index.ts
@@ -1,3 +1,7 @@
 export * from './Generic/IDelegate';
 export * from './Generic/IGenericDAO';
 export * from './Generic/GenericDAOImp';
+export * from './DAOImp/UserDAOImp';
+export * from './DAOImp/TrainingDAOImp';
+export * from './DAOImp/WeekDayPlanDAOImp';
+export * from './DAOImp/ExercismDAOImp';
